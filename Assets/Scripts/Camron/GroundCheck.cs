6c1e9ad7e35483cddb6ebe7ef2c0ded98b2dadee
Splatypus <lionheart365@gmail.com>
2017-02-20 18:48:42 -0500

    Player movement, double jumping, and gliding

---
+++ Assets/Scripts/Camron/GroundCheck.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+
+    int collisions = 0;
+    public Player p;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.tag == "Ground") {
+            collisions++;
+            p.canJump = true;
+
+            if (collisions == 0)
+                p.inAir = true;
+            else
+                p.inAir = false;
+
+        }
+    }
+    void OnTriggerExit2D(Collider2D coll) {
+        if (coll.tag == "Ground") {
+            collisions--;
+
+            if (collisions == 0)
+                p.inAir = true;
+            else
+                p.inAir = false;
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Camron/Player.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //upgrades
+    public bool hasDoubleJump;
+    public bool hasGlide;
+
+    //movement variables
+    public float inAirSlow;
+    public float glideSlow;
+    public float glideFallSpeed;
+    public float jumpForce;
+    public float maxSpeed;
+    public float walkingAcceleration;
+    public float walkingDecceleration;
+
+    //other stuff
+    public bool canJump = true;
+    public bool inAir;
+    public bool isGliding;
+    private Rigidbody2D body;
+    public GroundCheck gc;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate() {
+
+        if (isGliding && body.velocity.y < -glideFallSpeed) {
+            body.velocity = new Vector2(body.velocity.x, -glideFallSpeed);
+        }
+
+        //double jump
+        if (Input.GetKeyDown(KeyCode.Space) && canJump && inAir && hasDoubleJump) {
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+            canJump = false;
+            
+        } 
+        //first jump
+        else if (Input.GetKey(KeyCode.Space) && !inAir) {
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+            canJump = hasDoubleJump;
+        }
+
+        //left and right movement
+        //right
+        if (Input.GetKey(KeyCode.D)) {
+            if (body.velocity.x < maxSpeed) {
+                if (inAir) {
+                    body.velocity = new Vector2(body.velocity.x + (walkingAcceleration * Time.fixedDeltaTime), body.velocity.y);
+                } else if (isGliding) {
+                    body.velocity = new Vector2(body.velocity.x + (walkingAcceleration * Time.fixedDeltaTime * glideSlow), body.velocity.y);
+                } else {
+                    body.velocity = new Vector2(body.velocity.x + (walkingAcceleration * Time.fixedDeltaTime * inAirSlow), body.velocity.y);
+                }
+            }
+        }
+        //left
+        if (Input.GetKey(KeyCode.A)) {
+            if (-body.velocity.x < maxSpeed) {
+                if (inAir) {
+                    body.velocity = new Vector2(body.velocity.x - (walkingAcceleration * Time.fixedDeltaTime), body.velocity.y);
+                } else if (isGliding) {
+                    body.velocity = new Vector2(body.velocity.x - (walkingAcceleration * Time.fixedDeltaTime * glideSlow), body.velocity.y);
+                } else {
+                    body.velocity = new Vector2(body.velocity.x - (walkingAcceleration * Time.fixedDeltaTime * inAirSlow), body.velocity.y);
+                }
+            }
+        }
+
+        if (  !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) {
+            //drag here
+        }
+
+        //glide
+        if (Input.GetKey(KeyCode.LeftShift) && inAir) {
+            //if the glide has just started
+            if (!isGliding) {
+                
+            }
+
+            isGliding = true;
+        } else {
+            //if the glide has just ended:
+            if (isGliding) {
+                
+            }
+
+            isGliding = false;
+        }
+
+    }
+
+}
