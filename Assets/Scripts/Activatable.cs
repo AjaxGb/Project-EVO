551ee4920faa49afe0152e274f3d688a11ce0e79
AjaxGb <ajaxgb@gmail.com>
2017-02-22 11:18:10 -0500

    Refactored for generics

--- Assets/Scripts/Activatable.cs
+++ Assets/Scripts/Activatable.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Activatable : MonoBehaviour {
+public abstract class Activatable : MonoBehaviour {
 
 	public float highlightDistance = 3f;
 	public Animator highlightAnimator;
-	public float deactivateDistance = 5f;
 
-	private Player currWatchingPlayer;
-
-	private static int animationShowHighlight = Animator.StringToHash("Show Highlight");
-	private static int animationShowText = Animator.StringToHash("Show Text");
+	protected static int animationShowHighlight = Animator.StringToHash("Show Highlight");
 
 	public static List<Activatable> allInScene = new List<Activatable>();
 
@@ -20,34 +15,17 @@ public class Activatable : MonoBehaviour {
 		set { highlightAnimator.SetBool(animationShowHighlight, value); }
 	}
 
-	public bool ShowingText {
-		get { return highlightAnimator.GetBool(animationShowText); }
-		set { highlightAnimator.SetBool(animationShowText, value); }
-	}
+	public abstract bool CanActivate { get; }
 
-	public bool CanActivate { get { return !ShowingText; } }
-
-	public void Activate(Player p) {
-		ShowingText = true;
-		currWatchingPlayer = p;
-	}
+	public abstract void Activate(Player p);
 
 	// Use this for initialization
-	void Start() {
+	protected void Start() {
 		allInScene.Add(this);
 	}
 
-	void Update() {
-		if (currWatchingPlayer == null || Time.timeScale == 0) return;
-		Vector2 distToPlayer = (Vector2)currWatchingPlayer.transform.position - (Vector2)transform.position;
-		if (distToPlayer.sqrMagnitude > deactivateDistance * deactivateDistance) {
-			ShowingText = false;
-			currWatchingPlayer = null;
-		}
-	}
-
 	// Cleanup when destroyed
-	void OnDestroy() {
+	protected void OnDestroy() {
 		allInScene.Remove(this);
 	}
 }
---
+++ Assets/Scripts/ActivatableText.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ActivatableText : Activatable {
+
+	public float deactivateDistance = 5f;
+
+	private Player currWatchingPlayer;
+
+	protected static int animationShowText = Animator.StringToHash("Show Text");
+
+	public bool ShowingText {
+		get { return highlightAnimator.GetBool(animationShowText); }
+		set { highlightAnimator.SetBool(animationShowText, value); }
+	}
+
+	public override bool CanActivate { get { return !ShowingText; } }
+
+	public override void Activate(Player p) {
+		ShowingText = true;
+		currWatchingPlayer = p;
+	}
+
+	void Update() {
+		if (currWatchingPlayer == null || Time.timeScale == 0) return;
+		Vector2 distToPlayer = (Vector2)currWatchingPlayer.transform.position - (Vector2)transform.position;
+		if (distToPlayer.sqrMagnitude > deactivateDistance * deactivateDistance) {
+			ShowingText = false;
+			currWatchingPlayer = null;
+		}
+	}
+}
