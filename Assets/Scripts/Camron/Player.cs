eb7cc685f613cd8c6c45fee62caf753e095ce42f
AjaxGb <ajaxgb@gmail.com>
2017-02-21 19:00:55 -0500

    Better ground collision

--- Assets/Scripts/Camron/Player.cs
+++ Assets/Scripts/Camron/Player.cs
@@ -9,20 +9,22 @@ public class Player : MonoBehaviour {
     public bool hasGlide;
 
     //movement variables
-    public float inAirSlow;
-    public float glideSlow;
-    public float glideFallSpeed;
-    public float jumpForce;
-    public float maxSpeed;
-    public float walkingAcceleration;
-    public float walkingDecceleration;
+    public float inAirSlow = 0.5f;
+    public float glideSlow = 0.3f;
+    public float glideFallSpeed = 2f;
+    public float jumpForce = 10f;
+    public float maxSpeed = 10f;
+    public float walkingAcceleration = 40f;
+    public float walkingDecceleration = 20f;
+
+	public float groundDistance = 1.0f;
+	public LayerMask groundLayers;
 
     //other stuff
     public bool canJump = true;
     public bool inAir;
     public bool isGliding;
     private Rigidbody2D body;
-    public GroundCheck gc;
 
     private float _playerHealth; //Variable for the player's health total, ease updating of this and the UI at the same time
     public float PlayerHealth
@@ -50,6 +52,10 @@ public class Player : MonoBehaviour {
 	}
 
     void FixedUpdate() {
+		if (Time.timeScale == 0) return;
+
+		// TODO: Add more raycasts to sides.
+		inAir = Physics2D.Raycast(transform.position, Vector2.down, groundDistance, groundLayers);
 
         if (isGliding && body.velocity.y < -glideFallSpeed) {
             body.velocity = new Vector2(body.velocity.x, -glideFallSpeed);
