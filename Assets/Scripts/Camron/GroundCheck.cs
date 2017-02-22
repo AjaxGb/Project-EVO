0fca2d9235714cddf0f7886b999155e52a8ff219
AjaxGb <ajaxgb@gmail.com>
2017-02-22 13:05:11 -0500

    Better ground check

--- Assets/Scripts/Camron/GroundCheck.cs
+++ Assets/Scripts/Camron/GroundCheck.cs
@@ -7,38 +7,20 @@ public class GroundCheck : MonoBehaviour {
     int collisions = 0;
     public Player p;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     void OnTriggerEnter2D(Collider2D coll) {
         if (coll.tag == "Ground") {
             collisions++;
             p.canJump = true;
 
-            if (collisions == 0)
-                p.inAir = true;
-            else
-                p.inAir = false;
-
+			p.inAir = (collisions == 0);
         }
     }
+
     void OnTriggerExit2D(Collider2D coll) {
         if (coll.tag == "Ground") {
             collisions--;
 
-            if (collisions == 0)
-                p.inAir = true;
-            else
-                p.inAir = false;
-
-        }
+			p.inAir = (collisions == 0);
+		}
     }
-
 }
