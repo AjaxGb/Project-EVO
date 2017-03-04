cca49a69ab414269e0e23b908d0afcdaad169cab
Kyle Ring <ringk2@rpi.edu>
2017-03-04 14:20:52 -0500

    Some work on making enemies walk back and forth while noticing edges/walls

---
+++ Assets/Scripts/Kyle/Vec2toVec3ExtensionMethod.cs
@@ -0,0 +1,22 @@
+﻿/*/
+* Script by Devin Curry
+* www.Devination.com
+* www.youtube.com/user/curryboy001
+* Please like and subscribe if you found my tutorials helpful :D
+* Google+ Community: https://plus.google.com/communities/108584850180626452949
+* Twitter: https://twitter.com/Devination3D
+* Facebook Page: https://www.facebook.com/unity3Dtutorialsbydevin
+/*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vec2toVec3ExtensionMethod {
+
+    public static Vector2 toVector2(this Vector3 vec3)
+    {
+        return new Vector2(vec3.x, vec3.y);
+    }
+
+}
