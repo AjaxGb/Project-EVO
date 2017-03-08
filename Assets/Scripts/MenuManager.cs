9def02e6e99668c4feeedafba51614492a822f77
AjaxGb <ajaxgb@gmail.com>
2017-03-08 14:55:32 -0500

    Prepare for build

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Menu")) {
+			Application.Quit();
+		}
+	}
+}
