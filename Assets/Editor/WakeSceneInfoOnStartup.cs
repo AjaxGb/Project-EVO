76498c5bd4496025e84283a48f906ebdef77ed96
AjaxGb <ajaxgb@gmail.com>
2017-03-19 22:38:13 -0400

    Ensure that all SceneInfos are loaded at startup

---
+++ Assets/Editor/WakeSceneInfoOnStartup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class WakeSceneInfoOnStartup {
+
+	public static readonly string[] scenePaths = { "Assets/Scenes" };
+
+	static WakeSceneInfoOnStartup() {
+		foreach (string guid in AssetDatabase.FindAssets("t:SceneInfo", scenePaths)) {
+			AssetDatabase.LoadAssetAtPath<SceneInfo>(AssetDatabase.GUIDToAssetPath(guid));
+		}
+	}
+	
+}
