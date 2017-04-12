99e2e9f819ad31d694395424a89c4749a57b82c0
AjaxGb <ajaxgb@gmail.com>
2017-04-12 11:46:09 -0400

    Basic boss 1 room

---
+++ Assets/Scripts/Boss1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss1 : MonoBehaviour, IKillable, IDamageable {
+
+	public float health = 140;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public float TakeDamage(float amount) {
+		if (amount >= health) {
+			amount = health;
+			health = 0;
+			Kill();
+			return amount;
+		}
+		health -= amount;
+		return amount;
+	}
+
+	public void Kill() {
+		Destroy(transform.gameObject);
+	}
+}
