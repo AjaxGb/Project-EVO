93b33e8ac41aade8c09451168da124794309cca9
Kyle Ring <ringk2@rpi.edu>
2017-04-25 18:10:26 -0400

    Audio Sources for text spot highlights

--- Assets/Scripts/Activatable.cs
+++ Assets/Scripts/Activatable.cs
@@ -6,13 +6,23 @@ public abstract class Activatable : MonoBehaviour {
 	public float highlightDistance = 3f;
 	public Animator highlightAnimator;
 
-	protected static int animationShowHighlight = Animator.StringToHash("Show Highlight");
+    public AudioSource audioSource; //Audio Source to play sounds on activatables
+    public AudioClip previewSound; //Sound to play when the object is highlighted
+    public AudioClip deactivateSound; //Sound to play when object stops being highlighted
+
+    protected static int animationShowHighlight = Animator.StringToHash("Show Highlight");
 
 	public static List<Activatable> allInScene = new List<Activatable>();
 
 	public bool Highlighted {
 		get { return highlightAnimator.GetBool(animationShowHighlight); }
-		set { highlightAnimator.SetBool(animationShowHighlight, value); }
+		set {
+            if (value != highlightAnimator.GetBool(animationShowHighlight)) {
+                highlightAnimator.SetBool(animationShowHighlight, value);
+                audioSource.clip = value ? previewSound : deactivateSound; //Use the sound appropriate to whether the object is highlighted or un-highlighted on this change in value
+                audioSource.Play(); //Play the clip
+            }
+        }
 	}
 
 	public abstract bool CanActivate { get; }
