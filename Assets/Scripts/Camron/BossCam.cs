22558c4e228eb678ff876559be8759b6f1d5d992
AjaxGb <ajaxgb@gmail.com>
2017-04-28 01:28:42 -0400

    Updates to maps

--- Assets/Scripts/Camron/BossCam.cs
+++ Assets/Scripts/Camron/BossCam.cs
@@ -2,21 +2,21 @@
 
 public class BossCam : MonoBehaviour {
 	
-	public Transform playerTransform;
+	public Player player;
 
 	public float zoom = 2f; // Zoom in/out by this amount
     public float sensivity = 0.2f;
 
 	// Use this for initialization
 	void Start () {
-		if (playerTransform == null && SceneLoader.inst != null) playerTransform = SceneLoader.inst.player.transform;
+		if (player == null && SceneLoader.inst != null) player = SceneLoader.inst.player;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.timeScale == 0 || !SceneLoader.IsInCurrentScene(gameObject)) return;
+		if (Time.timeScale == 0 || !SceneLoader.IsInCurrentScene(gameObject) || !player.IsAlive) return;
 		CameraFollow.inst.SetFixedAngle(
-			transform.position + (playerTransform.position - transform.position) * sensivity,
+			transform.position + (player.transform.position - transform.position) * sensivity,
 			zoom);
 	}
 }
